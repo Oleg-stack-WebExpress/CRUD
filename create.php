5a99cfc6f2369222b7607b5171ac24313532238d
Your Name <you@example.com>
2025-03-29 14:34:51 +0700

    Удаление лишнего из  create.php

--- create.php
+++ create.php
@@ -5,26 +5,26 @@ createTableProducts();
 $errors = null;
 $product = null;
 
-    if (
-        array_key_exists('name', $_POST) && array_key_exists('category', $_POST)
-        && array_key_exists('article', $_POST) && array_key_exists('price', $_POST)
-        || array_key_exists('discounted', $_POST)
-    ) {
+if (
+    array_key_exists('name', $_POST) && array_key_exists('category', $_POST)
+    && array_key_exists('article', $_POST) && array_key_exists('price', $_POST)
+    || array_key_exists('discounted', $_POST)
+) {
 
-        $name = htmlspecialchars($_POST['name']);
-        $category = htmlspecialchars($_POST['category']);
-        $article = htmlspecialchars($_POST['article']);
-        $price = htmlspecialchars($_POST['price']);
-        $discounted = htmlspecialchars($_POST['discounted']);
+    $name = htmlspecialchars($_POST['name']);
+    $category = htmlspecialchars($_POST['category']);
+    $article = htmlspecialchars($_POST['article']);
+    $price = htmlspecialchars($_POST['price']);
+    $discounted = htmlspecialchars($_POST['discounted']);
 
 
-        if (insertProduct($name, $category, $article, $price, $discounted)) {
-            header('Location:/');
-        } else {
-            $errors = 'Ошибка добавления!';
-        }
-
+    if (insertProduct($name, $category, $article, $price, $discounted)) {
+        header('Location:/');
+    } else {
+        $errors = 'Ошибка добавления!';
     }
+
+}
 
 ?>
 
@@ -38,11 +38,11 @@ if ($errors) {
 ?>
 
 <form action="#" method="POST">
-    <input name="name" value="<?= $product ? $product['name'] : '' ?>" placeholder="Название" />
-    <input name="category" value="<?= $product ? $product['category'] : '' ?>" placeholder="Категория" />
-    <input name="article" value="<?= $product ? $product['article'] : '' ?>" placeholder="Артикул" />
-    <input name="price" value="<?= $product ? $product['price'] : '' ?>" placeholder="Цена" />
-    <input name="discounted" value="<?= $product ? $product['discounted'] : '' ?>" placeholder="Цена со скидкой" />
+    <input name="name" placeholder="Название" />
+    <input name="category" placeholder="Категория" />
+    <input name="article" placeholder="Артикул" />
+    <input name="price" placeholder="Цена" />
+    <input name="discounted" placeholder="Цена со скидкой" />
     <input type="submit" value="Создать">
 </form>
 
